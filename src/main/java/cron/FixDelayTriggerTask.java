4ea1d2a42a4abaa1cc95c7b17bfb7fc94c4d8316
weiping <wang_luo_pingping@163.com>
2014-07-11 20:14:10 +0800

    增加固定延迟任务

---
+++ src/main/java/cron/FixDelayTriggerTask.java
@@ -0,0 +1,19 @@
+package cron;
+
+public abstract class FixDelayTriggerTask implements TriggerTask{
+
+	public void trigger(long time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public long getTriggerTime() {
+		return 0;
+	}
+
+	public boolean canTrigger() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
