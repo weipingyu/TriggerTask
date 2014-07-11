dc2fbd8abf942523228c6f117a12cd0961244c33
weiping <wang_luo_pingping@163.com>
2014-07-11 17:38:17 +0800

    增加Cron任务

---
+++ src/main/java/cron/CronTriggerTask.java
@@ -0,0 +1,20 @@
+package cron;
+
+public class CronTriggerTask implements TriggerTask {
+
+	public void trigger(long time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public long getTriggerTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean canTrigger() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
