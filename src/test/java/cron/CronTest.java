282db9c90cabd20c07d8224f8a8b4ded208df2f5
weiping <wang_luo_pingping@163.com>
2014-07-14 16:42:38 +0800

    XXX

---
+++ src/test/java/cron/CronTest.java
@@ -0,0 +1,18 @@
+package cron;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import com.opensymphony.oscache.util.FastCronParser;
+
+public class CronTest {
+
+	public static void main(String[] args) throws ParseException {
+		FastCronParser fastCronParser = new FastCronParser("0 6 * * *");
+		System.out.println(DateFormatUtils.format(new Date(fastCronParser.getTimeBefore(System.currentTimeMillis())),"yyyy-MM-dd HH:mm:ss"));
+
+	}
+
+}
